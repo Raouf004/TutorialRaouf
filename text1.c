a094ddfa280b6f80a85ae4d8138f4e4560eb16b9
Boussada Abderraouf <abderraouf.boussada@gmail.Com>
2024-12-28 09:54:20 +0100

    second

--- text1.c
+++ text1.c
@@ -5,6 +5,12 @@ int main() {
     printf("Hello every one !!!!");
     for(i=0;i<10;i++) {
         printf("%d",i);
+        printf("\nhello :::");
     }
+    void ecrire() {
+        printf("hello9999");
+    }
+
+    ecrire();
     return 0;
 }
